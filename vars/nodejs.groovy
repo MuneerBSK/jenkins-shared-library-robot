013351f257b4009f64e5bf2d52c5822ba8d7ace8
MuneerBSK <muneerbsk96@gmail.com>
2024-02-10 17:36:54 -0500

    AutoPush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,22 +1,22 @@
 def lintChecks() {
     sh '''
-                echo Lint Checks for ${COMPONENT}
-                echo installing jslint
-                # echo npm install jslint
-                # ls -Ltr node_modules/jslint/bin/
-                # node_modules/jslint/bin/jslint.js server.js
-                echo performing lint checks for ${COMPONENT}
-                echo performing lint checks completed ${COMPONENT}
-       '''
+        echo "Lint Checks for ${COMPONENT}"
+        echo "installing jslint"
+        # npm install jslint
+        # ls -Ltr node_modules/jslint/bin/
+        # node_modules/jslint/bin/jslint.js server.js
+        echo "performing lint checks for ${COMPONENT}"
+        echo "performing lint checks completed ${COMPONENT}"
+    '''
 }
 
 def sonarChecks() {
     sh '''
-        sonar-scanner -Dsonar.host.url=http://172.31.2.205:9000 -Dsonar.sources=. -Dsonar.projectKey==${COMPONENT} _Dsonar.login=admin -Dsonar.password=password
+        sonar-scanner -Dsonar.host.url=http://172.31.2.205:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password
     '''
 }
 
-// call is the default function which will be cqlled whrn you call the filename
+// call is the default function which will be called when you call the filename
 def call() {
     pipeline {
         agent any
@@ -24,14 +24,14 @@ def call() {
             stage('Lint Checks') {
                 steps {
                     script {
-                         lintChecks()
+                        lintChecks()
                     }
                 }
             }
             stage('Sonar Checks') {
                 steps {
                     script {
-                         sonarChecks()
+                        sonarChecks()
                     }
                 }
             }
